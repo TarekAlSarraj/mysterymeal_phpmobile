0c07b9491e0b08511bace4a1a33ac38e1740a20d
root <tarekalsarraj99@gmail.com>
2021-06-16 08:29:35 +0000

    connection changed 2

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$db_host='localhost';
-$db_user='id16644001_mysterymealdb';
-$db_password='gmd$0RC09+{NCa%U';
-$db_database='id16644001_mysterymeal_db';
+$db_host='http://143.244.152.251/';
+$db_user='mysterymeal_user_2';
+$db_password='mysterymeal_62021';
+$db_database='mysterymealdb';
 try{
     $con=mysqli_connect($db_host,$db_user,$db_password,$db_database);
-    echo"Connected Successfully";
+    echo"Connected Successfully Tarek!";
 }catch(PDOException $exc){
     echo $exc->getMessage();
     die("couldn't connect");
